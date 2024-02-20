1691818df4e07b2a5352a9474b8b39576158fe5a
amypo-reactapp <reactapp@amypo.in>
2024-02-20 10:15:41 +0000

    User

---
+++ src/Skill/Image.js
@@ -0,0 +1,9 @@
+import REACTAPP from 'react'
+const Skill=()=>{
+    return(
+        <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1200px-React_Logo_SVG.svg.png"></img>
+        </div>
+    )
+}
+export default Skill
---
+++ src/Skill/Skill.js
@@ -0,0 +1,11 @@
+import REACTAPP from 'react'
+const Skill=()=>{
+    return(
+    <div>
+        <h6>"React Skill Session"</h6>
+        <p> React is a JavaScript library created by Facebook.React is a User Interface (UI) library.React is a tool for building UI components</p>
+
+        </div>
+    )
+}
+export default Skill
